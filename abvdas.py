39b7abb34bee4b3c150a0467c79d2a74195533bb
TheDino13 <108119649+TheDino13@users.noreply.github.com>
2024-12-09 00:08:07 +0100

    Update abvdas.py

--- abvdas.py
+++ abvdas.py
@@ -1,32 +1,28 @@
 import tsplib95 as tslib
 import pandas as pd
 
-import tsplib95
-import pandas as pd
-
 def read_tsp_with_pandas(file_path="E:\\xd\\docs\\kroA100.tsp"):
-   # Загрузка TSP файла
     problem = tsplib95.load(file_path)
-    # Преобразование координат узлов в DataFrame
+   
     if problem.node_coords:
         coordinates = pd.DataFrame( [(node, x, y) for node, (x, y) in problem.node_coords.items()], columns=["NodeID", "X", "Y"])
     else:
-        coordinates = pd.DataFrame(columns=["NodeID", "X", "Y"])  # Пустая таблица, если координат нет
+        coordinates = pd.DataFrame(columns=["NodeID", "X", "Y"])  
 
     return coordinates
 coordinates = read_tsp_with_pandas()
 
 
-print("Координаты узлов:\n")
+print("Coordinates:\n")
 print(coordinates)
 
 node_id = 5
 node_data = coordinates[coordinates["NodeID"] == node_id]
 if not node_data.empty:
     x, y = node_data.iloc[0]["X"], node_data.iloc[0]["Y"]
-    print(f"Координаты для узла {node_id}: X={x}, Y={y}\n")
+    print(f"Coordinates {node_id}: X={x}, Y={y}\n")
 else:
-    print(f"Узел {node_id} не найден.\n")
+    print(f" {node_id} not found.\n")
 
 def calculate_distance(node1, node2, coordinates):
     node1_data = coordinates[coordinates["NodeID"] == node1].iloc[0]
